16fbd151d4b9d0e4f06e56113e8f223ac9bb587f
Hambbuk <51123268+Hambbuk@users.noreply.github.com>
2020-09-21 21:00:44 +0900

    Update drone_yolo.py

--- drone_yolo.py
+++ drone_yolo.py
@@ -1,70 +1,136 @@
 import numpy as np
-import darknet
+import time
 import cv2
+import imutils
+from imutils.video import FPS
+from imutils.video import VideoStream
 
 
-def retbox(detections,i,frame) :
-	label = detections[i][0].decode('utf-8')
-	score = detections[i][1]
-	classes = labels_arr.index(label)
 
-	x1 = int(round((detections[i][2][0]) - (detections[i][2][2]/2.0))) # top left x1 
-	y1 = int(round((detections[i][2][1]) - (detections[i][2][3]/2.0))) # top left xy 
-	x2 = int(round((detections[i][2][0]) + (detections[i][2][2]/2.0))) # bottom right x2 
-	y2 = int(round((detections[i][2][1]) + (detections[i][2][3]/2.0))) # bottom right y2 
-                
-	box = np.array([x1,y1,x2,y2])
+OUTPUT_FILE='output.avi'
+LABELS_FILE='obj.names'
+CONFIG_FILE='yolov4-tiny-custom.cfg'
+WEIGHTS_FILE='yolov4-tiny-custom_10000.weights'
+CONFIDENCE_THRESHOLD=0.3
 
-	return label, score, box
+H=None
+W=None
 
-def pizza_status(y1,y2,frame) :
-	if y2 > frame.shape[0]*14/20 :
-		#pull
-		status = 'pull'
-		line_color_b = (0,0,255)
-		txt_color = (0,0,255)
-        
-	elif y1 < frame.shape[0]*6/20 :
-		#push
-		status = 'push'
-		line_color_f = (0,0,255)
-		txt_color = (0,0,255)
-        
-	elif y2 < frame.shape[0]*14/20 and x1 > rgb_img.shape[0]*6/20 :
-		#ok
-		status = 'ok'
-		txt_color = (0,255,0)
-
-	return status,line_color_f,line_color_b,txt_color
+fps = FPS().start()
 
 
-cap = cv2.VideoCapture(0)
-threshold = 0.9
 
-stauts = ''
-line_color_f,line_color_b,txt_color = (0,255,0),(0,255,0),(0,255,0)
+LABELS = open(LABELS_FILE).read().strip().split("\n")
 
+np.random.seed(4)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+	dtype="uint8")
+
+
+net = cv2.dnn.readNetFromDarknet(CONFIG_FILE, WEIGHTS_FILE)
+
+vs = cv2.VideoCapture(2)
+
+
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+cnt =0;
 while True:
-	ret, frame = cap.read()
-	detections = darknet.detect_image(net,meta, frame , thresh=.15)
-    
-	for i in range(len(detections)) :
-		label , score , box = retbox(detections,i,frame,labels_arr)
-		left,top,right,bottom=box
-		#-----Draw Bounding box-----
-		#cv2.putText(frame, label, (x1,y1), cv2.FONT_HERSHEY_SIMPLEX, 2, rgb, 3)
-		#cv2.rectangle(frame, (x1,y1), (x2,y2), rgb, 3)
-		#---------------------------
-		if label == 'pizza' :
-			status,line_color_f,line_color_b,txt_color = pizza_status(top,bottom,frame)
-        
-	cv2.putText(frame, status, (100,50), cv2.FONT_HERSHEY_SIMPLEX, 2, txt_color, 3)
-	cv2.line(frame,(int(frame.shape[0]*6/20),0),(int(frame.shape[1]*6/20),frame.shape[0]),line_color_f,2)
-	cv2.line(frame,(int(frame.shape[0]*14/20),0),(int(frame.shape[1]*14/20),frame.shape[0]),line_color_b,2)
-	cv2.imshow('frame',frame)
-	k = cv2.waitKey(1)
-	if k == 27 :
+	cnt+=1
+	print ("Frame number", cnt)
+	try:
+		(grabbed, image) = vs.read()
+	except:
+		break
+	blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+		swapRB=True, crop=False)
+	net.setInput(blob)
+	if W is None or H is None:
+		(H, W) = image.shape[:2]
+	layerOutputs = net.forward(ln)
+
+
+
+
+
+
+	# initialize our lists of detected bounding boxes, confidences, and
+	# class IDs, respectively
+	boxes = []
+	confidences = []
+	classIDs = []
+
+	# loop over each of the layer outputs
+	for output in layerOutputs:
+		# loop over each of the detections
+		for detection in output:
+			# extract the class ID and confidence (i.e., probability) of
+			# the current object detection
+			scores = detection[5:]
+			classID = np.argmax(scores)
+			confidence = scores[classID]
+
+			# filter out weak predictions by ensuring the detected
+			# probability is greater than the minimum probability
+			if confidence > CONFIDENCE_THRESHOLD:
+				# scale the bounding box coordinates back relative to the
+				# size of the image, keeping in mind that YOLO actually
+				# returns the center (x, y)-coordinates of the bounding
+				# box followed by the boxes' width and height
+				box = detection[0:4] * np.array([W, H, W, H])
+				(centerX, centerY, width, height) = box.astype("int")
+
+				# use the center (x, y)-coordinates to derive the top and
+				# and left corner of the bounding box
+				x = int(centerX - (width / 2))
+				y = int(centerY - (height / 2))
+
+				# update our list of bounding box coordinates, confidences,
+				# and class IDs
+				boxes.append([x, y, int(width), int(height)])
+				confidences.append(float(confidence))
+				classIDs.append(classID)
+
+	# apply non-maxima suppression to suppress weak, overlapping bounding
+	# boxes
+	idxs = cv2.dnn.NMSBoxes(boxes, confidences, CONFIDENCE_THRESHOLD,
+		CONFIDENCE_THRESHOLD)
+
+	# ensure at least one detection exists
+	if len(idxs) > 0:
+		cv2.imwrite("test.jpg",cv2.resize(image,(800, 600)))
+		print("img saved")
+		# loop over the indexes we are keeping
+		for i in idxs.flatten():
+			# extract the bounding box coordinates
+			(x, y) = (boxes[i][0], boxes[i][1])
+			(w, h) = (boxes[i][2], boxes[i][3])
+
+			color = [int(c) for c in COLORS[classIDs[i]]]
+
+			cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+			text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+			cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+				0.5, color, 2)
+			
+
+	# show the output image
+	cv2.imshow("output", cv2.resize(image,(800, 600)))
+
+	fps.update()
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("q"):
 		break
 
+fps.stop()
+
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
 cv2.destroyAllWindows()
-cap.release()
+
+# release the file pointers
+print("[INFO] cleaning up...")
+vs.release()
