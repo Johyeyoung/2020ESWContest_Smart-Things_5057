3cc326a58ea25c43b65af6eefb0d54df6fcdaa74
sun3850 <sun3850@naver.com>
2020-10-10 06:34:59 +0900

    Update mqtt_publisher.py

--- turtlebot/src/mqtt_publisher.py
+++ turtlebot/src/mqtt_publisher.py
@@ -10,7 +10,7 @@ class MQTT_publisher:
     def __init__(self):
         self.mqtt_client = mqtt.Client("OTP")
 
-    def otp_value_publish(pos):
+    def otp_value_publish(self, pos):
         # MQTT client 생성, 이름
         self.mqtt_client.connect("192.168.0.15", 1883)  # 로컬호스트에 있는 MQTT서버에 접속
         self.mqtt_client.publish("otp_result", pos)  # topic 과 넘겨줄 값
